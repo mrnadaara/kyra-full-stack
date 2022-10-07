3d55dd6f3ff64562460b30a3ae83460b02a72f08
mrnadaara <s.mustaf.m@gmail.com>
2022-10-07 16:22:47 +0100

    added exception test case for unit test

--- backend/tests/unit/services/foursquare.test.js
+++ backend/tests/unit/services/foursquare.test.js
@@ -3,7 +3,6 @@ const nock = require('nock');
 const { foursquareService } = require('../../../src/services');
 const config = require('../../../src/config/config');
 const placesDummyData = require('../../../src/data/places_dummy_data.json');
-const ApiError = require('../../../src/utils/ApiError');
 
 describe('Foursquare services', () => {
   describe('getNearByPlaces', () => {
@@ -27,24 +26,14 @@ describe('Foursquare services', () => {
       expect(places.results[0]).toEqual(placesDummyData.results[0]);
     });
 
-    test('should return 502 error if foursquare api returns an error', () => {
-      // mock external api call to foursquare places
-      nock(config.foursquare_api.url)
-        .get('/places/search')
-        .query({
-          ll: '12.345678,-0.123456',
-          sort: 'DISTANCE',
-          categories: '17000',
-        })
-        .reply(400);
-
-      expect(async () => {
-        await foursquareService.getNearbyPlaces({
+    test('should return APIerror when exception is raised', async () => {
+      await expect(() =>
+        foursquareService.getNearbyPlaces({
           lat: '12.345678',
           lon: '-0.123456',
           categories: '15000',
-        });
-      }).toThrow(ApiError);
+        })
+      ).rejects.toThrow('Could not retrieve places');
     });
   });
 });
